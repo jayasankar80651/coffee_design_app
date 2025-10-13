801d7b7be8b49da6150df99540332992c0fc803f
jayasnkar80651 <jay80651@gmail.com>
2025-10-13 22:23:24 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/coffee_design/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.coffee_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/newpage.dart
@@ -0,0 +1,35 @@
+import'package:flutter/material.dart';
+class CoffeeDesign extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(backgroundColor: Colors.black,
+      body:Column(
+        children: [
+          Container(
+            height: 400,
+            width:550,
+            decoration: BoxDecoration(image:DecorationImage(
+              image:AssetImage("assets/images/coffedelight.jpeg") ),
+            ),
+          ),
+          Positioned(
+            child:  Text("Fall in love with \nCoffee in Blissful\n      Delight!",
+            style: TextStyle(color: Colors.white,fontSize: 29,fontWeight: FontWeight.bold),
+          )),
+          SizedBox(height: 10,),
+          Positioned(child: Text("welcome to our cozy coffee corner,where\n      every cup is a delightful for you.",
+          style: TextStyle(color: Colors.grey),)),
+          SizedBox(height: 20,),
+        Container(child:
+         Container(
+          height: 50,
+          width: 270,
+          decoration: BoxDecoration(borderRadius:BorderRadius.circular(10),color:  Colors.orange),
+          child: Center(child: Text("Get Started",style: TextStyle(fontSize: 15,color:Colors.white),)),
+         )
+        )
+        ],
+      )
+    );
+  }
+}
